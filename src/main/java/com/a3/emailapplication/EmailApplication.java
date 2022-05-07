18f0bdc11ca1ae44df95898666747194ddd57972
Alexandra Pop <alexandra.pop201@gmail.com>
2022-05-07 21:21:30 +0300

    first commit

---
+++ src/main/java/com/a3/emailapplication/EmailApplication.java
@@ -0,0 +1,13 @@
+package com.a3.emailapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a3/emailapplication/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3.emailapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
